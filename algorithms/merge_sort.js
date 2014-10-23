41571a495cd9df48c0614f171a96a902256a57a0
Varun Kashyap <varunkashyapcse@gmail.com>
2014-10-22 17:34:49 -0700

    Merge sort implementation. Only has auxiliary merge for now

---
+++ algorithms/merge_sort.js
@@ -0,0 +1,53 @@
+function MergeSort(a, options) {
+    var sort, merge_aux, merge, merge_in_place, aux, less;
+    
+    options.merge_in_place = options.merge_in_place !== false;
+    this.a = a;
+
+    this.run = function() {
+        aux = [];
+        sort(a, 0, a.length - 1);
+        return a;
+    };
+
+    sort = function(a, lo, hi) {
+        if( hi <= lo) return;
+
+        var mid = Math.floor(lo + (hi - lo) / 2);
+        sort(a, lo, mid);
+        sort(a, mid + 1, hi);
+        merge(a, lo, mid, hi);
+    };
+
+    // Merging using auxiliary array
+    merge_aux = function (a, lo, mid, hi) {
+        var i = lo, j = mid + 1;
+
+        // copy into auxiliary array
+        for(var k = lo; k <= hi; k++) {
+            aux[k] = a[k];
+        }
+
+        for(k = lo; k <= hi; k++) {
+            if (i > mid) {
+                a[k] = aux[j];
+                j++
+            } else if (j > hi) {
+                a[k] = aux[i];
+                i++
+            } else if (less(aux[j], aux[i])) {
+                a[k] = aux[j];
+                j++;
+            } else {
+                a[k] = aux[i];
+                i++;
+            }
+        }
+    };
+
+    less = function(a, b) {
+        return a < b;
+    };
+
+    merge = options.merge_in_place ? merge_in_place : merge_aux;
+}
